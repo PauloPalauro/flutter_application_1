f6daaf58a1555b74e9963c2e16de075e75b213f2
PauloEduardoPalauro <paulopalauro@gmail.com>
2024-10-25 21:56:36 -0300

    List and Download files from storage firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,229 +1,274 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_mjpeg/flutter_mjpeg.dart';
-import 'package:web_socket_channel/io.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
-import 'dart:convert';
 import 'dart:async';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:pdfx/pdfx.dart';
 
-class WebcamStreamScreen extends StatefulWidget {
-  @override
-  _WebcamStreamScreenState createState() => _WebcamStreamScreenState();
+void main() async {
+  await dotenv.load(fileName: "/home/ideal_pad/Documentos/Projetos/teste_flutter/flutter_application_1/.env");
+  runApp(MyApp());
 }
 
-class _WebcamStreamScreenState extends State<WebcamStreamScreen> {
-  String streamUrl = 'http://192.168.0.14:8001/video_feed';
-  late WebSocketChannel channel;
-  String statusMessage = 'Nenhuma mensagem recebida';
-  String SecurityMessage = "";
-  String? base64Image;
-  Color securityMessageColor = Colors.red;
-
-  int? countdown; // Variável para a contagem regressiva
-
+class MyApp extends StatelessWidget {
   @override
-  void initState() {
-    super.initState();
-    channel = IOWebSocketChannel.connect('ws://192.168.0.14:8001/ws');
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StorageListPage(),
+    );
+  }
+}
 
-    // Escuta as mensagens recebidas do WebSocket
-    channel.stream.listen((message) {
-      if (message.trim().toLowerCase().startsWith("img:")) {
-        setState(() {
-          base64Image = message.substring(4).trim();
-        });
-      } else if (message.trim().toLowerCase().startsWith("msg:")) {
-        String msg = message.substring(4).trim();
-        setState(() {
-          statusMessage = msg;
-        });
+class StorageListPage extends StatefulWidget {
+  @override
+  _StorageListPageState createState() => _StorageListPageState();
+}
 
-        // Iniciar contagem regressiva dependendo do conteúdo da mensagem
-        if (msg.contains("Analise volta em 10") || msg.contains("Pessoa Detectada. Tirando foto em 10 segundos") ) {
-          setState(() {
-            countdown = 10;
-          });
-        } 
-      } else if (message.trim().toLowerCase().startsWith("sec:")) {
-        String secMessage = message.substring(4).trim();
-        setState(() {
-          SecurityMessage = secMessage;
-          securityMessageColor = secMessage.contains("Todos os itens de segurança presentes para ")
-              ? Colors.green
-              : Colors.red;
-        });
+class _StorageListPageState extends State<StorageListPage> {
+  List<Map<String, dynamic>> items = [];
+  List<Map<String, dynamic>> filteredItems = [];
+  bool isListVisible = false;
+  bool isDownloading = false;
+  String searchQuery = '';
+
+  final String bucketUrl = dotenv.env['BUCKET_URL'] ?? '';
+
+  Future<void> fetchItems() async {
+    final response = await http.get(Uri.parse(bucketUrl));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final List<Map<String, dynamic>> fetchedItems = List<Map<String, dynamic>>.from(
+        data['items'].map((item) => {
+          'name': item['name'],
+          'downloadUrl': '$bucketUrl/${Uri.encodeComponent(item['name'])}?alt=media',
+        }),
+      );
+
+      setState(() {
+        items = fetchedItems;
+        filteredItems = fetchedItems;
+        isListVisible = true;
+      });
+    } else {
+      print('Erro ao buscar itens: ${response.statusCode}');
+    }
+  }
+
+  Future<void> downloadFile(String url, String fileName) async {
+    setState(() => isDownloading = true);
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final directory = await getApplicationDocumentsDirectory();
+        final filePath = '${directory.path}/$fileName';
+        final file = File(filePath);
+        await file.writeAsBytes(response.bodyBytes);
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Arquivo $fileName baixado com sucesso!')),
+        );
       } else {
-        setState(() {
-          statusMessage = message.trim();
-        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Erro ao baixar o arquivo')),
+        );
       }
+    } catch (e) {
+      print('Erro ao baixar arquivo: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro ao baixar o arquivo')),
+      );
+    } finally {
+      setState(() => isDownloading = false);
+    }
+  }
+
+  void filterItems(String query) {
+    setState(() {
+      searchQuery = query;
+      filteredItems = items
+          .where((item) =>
+              item['name'].toLowerCase().contains(query.toLowerCase()))
+          .toList();
     });
   }
 
-  @override
-  void dispose() {
-    channel.sink.close();
-    super.dispose();
+  void toggleListVisibility() {
+    if (isListVisible) {
+      setState(() {
+        isListVisible = false;
+        filteredItems = [];
+      });
+    } else {
+      fetchItems();
+    }
+  }
+
+  void openFileContent(String downloadUrl, String fileName) async {
+    final response = await http.get(Uri.parse(downloadUrl));
+    if (response.statusCode == 200) {
+      final fileData = response.bodyBytes;
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => FileContentScreen(fileName: fileName, fileData: fileData),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro ao carregar conteúdo do arquivo')),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Webcam Streaming'),
+        title: Row(
+          children: [
+            Icon(Icons.storage, color: Colors.white),
+            SizedBox(width: 8),
+            Text('Itens do Bucket Firebase'),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  margin: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.3),
-                        blurRadius: 15,
-                        offset: Offset(0, 4),
-                      ),
-                    ],
-                  ),
-                  child: Mjpeg(
-                    stream: streamUrl,
-                    isLive: true,
-                    error: (context, error, stack) {
-                      return Center(child: Text('Erro ao carregar o stream'));
-                    },
-                  ),
-                ),
+      body: Container(
+        color: Colors.black,
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: filterItems,
+              decoration: InputDecoration(
+                labelText: 'Pesquisar',
+                prefixIcon: Icon(Icons.search),
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
               ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  margin: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.3),
-                        blurRadius: 15,
-                        offset: Offset(0, 4),
-                      ),
-                    ],
-                  ),
-                  child: base64Image != null
-                      ? Image.memory(
-                          Base64Decoder().convert(base64Image!),
-                          fit: BoxFit.cover,
-                        )
-                      : Center(
-                          child: Text(
-                            'Nenhuma imagem recebida',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
+              style: TextStyle(color: Colors.black),
+            ),
+            SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: toggleListVisibility,
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                elevation: 2,
+              ),
+              child: Text(isListVisible ? 'Ocultar Itens do Bucket' : 'Mostrar Itens do Bucket'),
+            ),
+            SizedBox(height: 12),
+            Expanded(
+              child: isListVisible
+                  ? ListView.builder(
+                      itemCount: filteredItems.length,
+                      itemBuilder: (context, index) {
+                        final item = filteredItems[index];
+                        return Card(
+                          elevation: 4,
+                          margin: EdgeInsets.symmetric(vertical: 6),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                        ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 5),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0),
-            child: Text(
-              statusMessage,
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.white,
-              ),
-              textAlign: TextAlign.center,
+                          child: ListTile(
+                            leading: getFileIcon(item['name']),
+                            title: GestureDetector(
+                              onTap: () => openFileContent(item['downloadUrl'], item['name']),
+                              child: Text(
+                                item['name'],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.blue,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                            trailing: isDownloading
+                                ? CircularProgressIndicator()
+                                : TextButton(
+                                    onPressed: () => downloadFile(item['downloadUrl'], item['name']),
+                                    child: Text('Download'),
+                                  ),
+                          ),
+                        );
+                      },
+                    )
+                  : Center(
+                      child: Text(
+                        'Pressione "Mostrar" para exibir os itens',
+                        style: TextStyle(color: Colors.grey.shade600),
+                      ),
+                    ),
             ),
-          ),
-          // Exibe o widget da contagem regressiva se `countdown` não for nulo
-          if (countdown != null)
-            CountdownWidget(
-              initialCountdown: countdown!,
-              onCountdownComplete: () {
-                setState(() {
-                  countdown = null;
-                });
-              },
-            ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 80.0),
-            child: Text(
-              SecurityMessage,
-              style: TextStyle(fontSize: 20, color: securityMessageColor),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
+
+  Icon getFileIcon(String fileName) {
+    if (fileName.toLowerCase().endsWith('.pdf')) {
+      return Icon(Icons.picture_as_pdf, color: Colors.red);
+    } else if (fileName.toLowerCase().endsWith('.jpg') ||
+               fileName.toLowerCase().endsWith('.jpeg') ||
+               fileName.toLowerCase().endsWith('.png') ||
+               fileName.toLowerCase().endsWith('.gif')) {
+      return Icon(Icons.image, color: Colors.blue);
+    } else {
+      return Icon(Icons.insert_drive_file, color: Colors.grey);
+    }
+  }
 }
 
-class CountdownWidget extends StatefulWidget {
-  final int initialCountdown;
-  final VoidCallback onCountdownComplete;
+class FileContentScreen extends StatelessWidget {
+  final String fileName;
+  final List<int> fileData;
 
-  CountdownWidget({
-    required this.initialCountdown,
-    required this.onCountdownComplete,
-  });
+  FileContentScreen({required this.fileName, required this.fileData});
 
-  @override
-  _CountdownWidgetState createState() => _CountdownWidgetState();
-}
-
-class _CountdownWidgetState extends State<CountdownWidget> {
-  late int countdown;
-  Timer? timer;
-
-  @override
-  void initState() {
-    super.initState();
-    countdown = widget.initialCountdown;
-    timer = Timer.periodic(Duration(seconds: 1), (Timer t) {
-      setState(() {
-        if (countdown > 0) {
-          countdown -= 1;
-        } else {
-          t.cancel();
-          widget.onCountdownComplete(); // Chama o callback quando a contagem termina
-        }
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    timer?.cancel();
-    super.dispose();
-  }
+  bool get isSupportedPlatform => Platform.isAndroid || Platform.isIOS || Platform.isWindows;
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'Contagem regressiva: $countdown segundos',
-      style: TextStyle(
-        fontSize: 18,
-        color: Colors.orange,
+    bool isImage = fileName.toLowerCase().endsWith('.jpg') ||
+                   fileName.toLowerCase().endsWith('.jpeg') ||
+                   fileName.toLowerCase().endsWith('.png') ||
+                   fileName.toLowerCase().endsWith('.gif');
+
+    bool isText = fileName.toLowerCase().endsWith('.txt');
+    bool isPdf = fileName.toLowerCase().endsWith('.pdf');
+
+    return Scaffold(
+      appBar: AppBar(title: Text(fileName)),
+      body: Center(
+        child: isImage
+            ? Image.memory(Uint8List.fromList(fileData))
+            : isText
+                ? Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SingleChildScrollView(
+                      child: Text(
+                        utf8.decode(fileData),
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                  )
+                : isPdf && isSupportedPlatform
+                    ? PdfViewPinch(
+                        controller: PdfControllerPinch(
+                          document: PdfDocument.openData(fileData as FutureOr<Uint8List>),
+                        ),
+                      )
+                    : Text(
+                        isPdf ? 'Visualização de PDF não suportada neste dispositivo' : 'Formato de arquivo não suportado',
+                        style: TextStyle(color: Colors.white),
+                      ),
       ),
     );
   }
 }
-
-void main() => runApp(MaterialApp(
-      home: WebcamStreamScreen(),
-      theme: ThemeData.dark(),
-    ));
